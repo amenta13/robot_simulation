3aefe89e31404a4446a265103ef16d9fc1f0f501
rnwoodruff <ryan.t.woodruff@gmail.com>
2025-10-30 08:59:57 -0400

    updated index.php with footer and stylesheet ref

--- webpage/index.php
+++ webpage/index.php
@@ -2,17 +2,21 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
+        <link href="style.css" rel="stylesheet">
         <title>Coming Soon!</title>
-        <body>
-            <h1 style="text-align:center;font-weight:bold;font-size: 120px;color:#3498db;">Coming Soon!</h1>
-            <h2 style="text-align:center;font-weight:bold;font-size: 35px;color:#084873;">We're working on it.</h2>
-            <nav>
-                <a href="controls.php">Controls</a>
-            </nav>
-            <nav>
-                <a href="about.php">About</a>
-            </nav>
-            <img src="./robotic-arm.gif" alt="" style="width:200px;height:auto;display:block;margin:0 auto;">
-        </body>
+        <nav>
+            <a href="controls.php">Controls</a>
+        </nav>
+        <nav>
+            <a href="about.php">About</a>
+        </nav>
     </head>
+    <body>
+        <h1 style="text-align:center;font-weight:bold;font-size: 120px;color:#3498db;">Coming Soon!</h1>
+        <h2 style="text-align:center;font-weight:bold;font-size: 35px;color:#084873;">Explore the About and Controls Pages!</h2>
+        <img src="./robotic-arm.gif" alt="" style="width:200px;height:auto;display:block;margin:0 auto;">
+    </body>
+    <footer>
+        <h1 id="Footer">Designed by Andrew Ament, Hunter Rohrbaugh, Braden Scott, Ryan Woodruff</h1>
+    </footer>
 </html>
