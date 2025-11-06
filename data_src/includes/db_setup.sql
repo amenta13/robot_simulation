88ebe593026c43e4f8c4a8e607a6fc6b3ec7068c
rnwoodruff <ryan.t.woodruff@gmail.com>
2025-11-06 10:25:22 -0500

    created database setup file with sql commands for creating tables

---
+++ data_src/includes/db_setup.sql
@@ -0,0 +1,57 @@
+CREATE TABLE IF NOT EXISTS  `User` (
+  `UserID` INT NOT NULL,
+  `Name` VARCHAR(45) NULL,
+  `Password` VARCHAR(45) NULL,
+  `RequestTime` VARCHAR(45) NULL,
+  `ObjectID` INT NULL,
+  `InsID` INT NULL,
+  PRIMARY KEY (`UserID`))
+ENGINE = InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS  `Object` (
+  `ObjectID` INT NOT NULL,
+  `User_UserID` INT NOT NULL,
+  `Height` INT NULL,
+  `Weight` INT NULL,
+  `PickUpLoc` INT NULL,
+  `DropLoc` INT NULL,
+  PRIMARY KEY (`ObjectID`),
+  INDEX `fk_Object_User_idx` (`User_UserID` ASC) VISIBLE,
+  CONSTRAINT `fk_Object_User`
+    FOREIGN KEY (`User_UserID`)
+    REFERENCES  `User` (`UserID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS  `Robot` (
+  `RobotID` INT NOT NULL,
+  `Status` VARCHAR(45) NULL,
+  `Battery` INT NULL,
+  PRIMARY KEY (`RobotID`))
+ENGINE = InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS  `Instruction` (
+  `InsID` INT NOT NULL,
+  `User_UserID` INT NOT NULL,
+  `Robot_RobotID` INT NOT NULL,
+  `Instruction` VARCHAR(45) NULL,
+  `Status` VARCHAR(45) NULL,
+  `Log` VARCHAR(45) NULL,
+  PRIMARY KEY (`InsID`),
+  INDEX `fk_Instruction_User1_idx` (`User_UserID` ASC) VISIBLE,
+  INDEX `fk_Instruction_Robot1_idx` (`Robot_RobotID` ASC) VISIBLE,
+  CONSTRAINT `fk_Instruction_User1`
+    FOREIGN KEY (`User_UserID`)
+    REFERENCES  `User` (`UserID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Instruction_Robot1`
+    FOREIGN KEY (`Robot_RobotID`)
+    REFERENCES  `Robot` (`RobotID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
