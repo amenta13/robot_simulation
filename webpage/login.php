12a32157a46ed5a0de46adbe2efdcb6c3ecceae9
hunterrohrbaugh <hsrohrbaugh@gmail.com>
2025-11-06 09:53:05 -0500

    added login page

--- webpage/login.php
+++ webpage/login.php
@@ -21,7 +21,18 @@ require_once 'web_elements/navbar.php';
         </nav>
     </head>
     <body>
-
+        <div class="login-container">
+            <h2>Login</h2>
+            <form action="authenticate.php" method="POST">
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required>
+                
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required>
+                
+                <button type="submit">Login</button>
+            </form>
+        </div>
     </body>
 </html>
 
