90cdfd56d7aa42b8b68be4829f7f9667c6de4269
amenta13 <andrewgsoccer2004@gmail.com>
2025-11-12 10:23:38 -0500

    Added a wave python function file

--- data_src/control_scripts/wave.py
+++ data_src/control_scripts/wave.py
@@ -5,15 +5,18 @@ mc = MyCobot('/dev/ttyAMA0', 1000000)
 cycles = 3
 delay = 0.25
 neutral = [0, 0, 0, 0, 0, 0]
+adjustwrist = [0, 0, 0, 0, -90, 0]
 mc.send_angles(neutral, 50)
+time.sleep(1)
+mc.send_angles(adjustwrist, 50)
 time.sleep(1)
 
 for c in range(cycles):
 
-    mc.send_angles([0, 15, -30, 15, 0, 0], 50)
+    mc.send_angles([0, 15, 15, 15, -90, 0], 50)
     time.sleep(delay)
 
-    mc.send_angles([0, -15, 30, -15, 0, 0], 50)
+    mc.send_angles([0, -15, -15, -15, -90, 0], 50)
     time.sleep(delay)
 
 mc.send_angles(neutral, 50)
