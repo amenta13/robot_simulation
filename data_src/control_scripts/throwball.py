4feeeab40d213154b0d5dd43e504e89c3622c440
amenta13 <andrewgsoccer2004@gmail.com>
2025-11-13 10:07:30 -0500

    Added throwball function

--- data_src/control_scripts/throwball.py
+++ data_src/control_scripts/throwball.py
@@ -1,13 +1,22 @@
 from pymycobot.mycobot import MyCobot
 import time
 
-mc = MyCobot('/dev/ttyAMA0',1000000)
-mc.send_angles([(-80),(-35),(-90),20,0,0],50)
-time.sleep(1)
-mc.set_gripper_value(0,100)
-time.sleep(1)
-mc.send_angles([0,0,0,0,0,0],50)
-time.sleep(1)
-mc.send_angles([(-80),(-35),(-90),20,0,0],50)
-time.sleep(1)
-mc.set_gripper_value(100,100)
+mc = MyCobot('/dev/ttyAMA0', 1000000)
+delay = 1
+neutral = [0, 0, 0, 0, 0, 0]
+pose1 = [90, 90, 0, 0, -90, 90]
+pose2 = [90, 0, 0, 0, -90, 90]
+pose3 = [90, -90, 0, 0, -90, 90]
+
+mc.send_angles(neutral, 50)
+time.sleep(delay)
+
+mc.send_angles(pose1, 50)
+time.sleep(delay)
+mc.send_angles(pose2, 50)
+time.sleep(delay)
+mc.send_angles(pose3, 50)
+time.sleep(delay)
+
+mc.send_angles(neutral, 50)
+time.sleep(delay)
