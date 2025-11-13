3e37c8cec7faa2259a23c2c663d71ce5015d7431
rnwoodruff <ryan.t.woodruff@gmail.com>
2025-11-13 08:36:47 -0500

    Added required programs for connecting to the database such as db_config and db_connect. Also added commands.txt for recording all commands that are run in the robot PI.

--- data_src/includes/db_config.php
+++ data_src/includes/db_config.php
@@ -1,5 +1,15 @@
 <?php
-// File for configuring database
-
+global $host, $database, $db_user, $db_pass;
+if($_SERVER["HTTP_HOST"]=="127.0.0.1" || $_SERVER["HTTP_HOST"]=="localhost"){
+    $host = "localhost";
+    $database = "triviagames";
+    $db_user = "root";
+    $db_pass = "";
+}else{
+    $host = "srv557.hstgr.io";
+    $database = "u413142534_robotworkdb";
+    $db_user = "u413142534_robotwork";
+    $db_pass = "";
+}
 
 ?>
--- data_src/includes/db_connect.php
+++ data_src/includes/db_connect.php
@@ -1,5 +1,9 @@
 <?php
-// File for connecting to database
+    require_once './db_config.php';
 
+    $conn = new mysqli($host, $db_user, $db_pass, $database);
 
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 ?>
